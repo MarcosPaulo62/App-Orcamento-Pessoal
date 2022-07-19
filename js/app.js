3836bdf3c2a09e9a1cdcb8d006a48e1e98d4bec0
MarcosPaulo62 <markossylva62@gmail.com>
2022-07-19 15:47:02 -0300

    listando despesas na tela de consulta

--- js/app.js
+++ js/app.js
@@ -21,6 +21,22 @@ class Bd {
 
         localStorage.setItem('id', id)
     }
+
+    recuperarTodosRegistros() {
+        let id = localStorage.getItem('id')
+        let despesas = []
+
+        // recuperando todas as despesas
+        for(let i = id; i >= 1; i--) {
+            let despesa = JSON.parse(localStorage.getItem(i))
+
+            if(despesa != null){
+                despesas.push(despesa)
+            }
+        }
+
+        return despesas
+    }
 }
 
 let bd = new Bd()
@@ -55,6 +71,13 @@ function cadastrarDespesa() {
     if(gravar) {
         bd.gravar(despesa)
 
+        ano.value = ''
+        mes.value = ''
+        dia.value = ''
+        tipo.value = ''
+        descricao.value = ''
+        valor.value = ''
+
         document.getElementById('tituloModal').innerHTML = 'Registro inserido com sucesso'
         document.getElementById('descricaoModal').innerHTML = 'Despesa foi cadastrada com sucesso!'
         document.getElementById('botaoModal').innerHTML = 'Voltar'
@@ -73,5 +96,52 @@ function cadastrarDespesa() {
         // Dialog de erro
         $('#modalRegistraDespesa').modal('show')
     }
+}
+
+function carregaListaDespesas() {
+    let despesas = bd.recuperarTodosRegistros() // Array das despesas
+
+    let listaDespesas = document.getElementById('listaDespesas') // body da tabela
+
+    /*
+    <tr>
+        <td>15/03/2018</td>
+        <td>Alimentação</td>
+        <td>Compras do mês</td>
+        <td>500.00</td>
+    </tr>
+    */
+
+    // Percorrendo despesas
+    despesas.forEach( x => {
+
+        // criando a linha (tr)
+        let linha = listaDespesas.insertRow()
+
+        // definindo tipo com base no número correspondente
+        switch (x.tipo) {
+            case '1':
+                x.tipo = 'Alimentação'
+                break;
+            case '2':
+                x.tipo = 'Educação'
+                break;
+            case '3':
+                x.tipo = 'Lazer'
+                break;
+            case '4':
+                x.tipo = 'Saúde'
+                break;
+            case '5':
+                x.tipo = 'Transporte'
+                break;
+        }
+        
+        // criar as colunas (td)
+        linha.insertCell(0).innerHTML = `${x.dia}/${x.mes}/${x.ano}`
+        linha.insertCell(1).innerHTML = x.tipo
+        linha.insertCell(2).innerHTML = x.descricao
+        linha.insertCell(3).innerHTML = x.valor
+    })
 }
  
